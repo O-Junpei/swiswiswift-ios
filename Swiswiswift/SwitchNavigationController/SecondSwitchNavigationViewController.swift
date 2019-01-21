67ad6b146613f30e6fa4d1c08ddf6de594ee87af
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-01-22 02:11:13 +0900

    TableViewなどのソースを追加

---
+++ Swiswiswift/SwitchNavigationController/SecondSwitchNavigationViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class SecondSwitchNavigationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "SecondView"
+        view.backgroundColor = UIColor.blue
+        
+        let width = view.frame.width
+        let height = view.frame.height
+        
+        let transitionBtn = UIButton()
+        transitionBtn.frame.size = CGSize(width: width * 0.6, height: height * 0.2)
+        transitionBtn.center = view.center
+        transitionBtn.backgroundColor = UIColor.green
+        transitionBtn.setTitle("Switch to FirstVC", for: UIControl.State.normal)
+        transitionBtn.setTitleColor(UIColor.black, for: UIControl.State.normal)
+        transitionBtn.addTarget(self, action: #selector(backToFirstVC(sender:)), for:.touchUpInside)
+        view.addSubview(transitionBtn)
+    }
+    
+    @objc internal func backToFirstVC(sender: UIButton){
+        let firstVC = FirstSwitchNavigationViewController()
+        navigationController?.setViewControllers([firstVC], animated: true)
+    }
+}
