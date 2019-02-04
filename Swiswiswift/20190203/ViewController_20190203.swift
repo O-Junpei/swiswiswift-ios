d1462280ebd19e0454b466b43f07efec27142769
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-02-04 22:44:21 +0900

    Write to FireStore

--- Swiswiswift/20190203/ViewController_20190203.swift
+++ Swiswiswift/20190203/ViewController_20190203.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class ViewController_20190203: UIViewController {
 
@@ -6,5 +7,47 @@ class ViewController_20190203: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        // Document
+        // https://firebase.google.com/docs/firestore/quickstart
+        
+        let db = Firestore.firestore()
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "first": "Ada",
+            "last": "Lovelace",
+            "born": 1815
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+
+        // Add a second document with a generated ID.
+        ref = db.collection("users").addDocument(data: [
+            "first": "Alan",
+            "middle": "Mathison",
+            "last": "Turing",
+            "born": 1912
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
+        db.collection("users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+        
     }
 }
